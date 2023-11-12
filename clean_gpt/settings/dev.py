83a1873e754570c832725c106b441e422b0507f1
amit7 <utilitiesofamit@gmail.com>
2023-11-13 00:18:55 +0530

    Initial commit

---
+++ clean_gpt/settings/dev.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = "django-insecure-+@+n7=)ixsdcuaqbq351to^6xf^$+_)yt5%k45b%cx%%v)mng+"
---
+++ clean_gpt/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+SECRET_KEY = "django-insecure-+@+n7=)ixsdcuaqbq351to^6xf^$+_)yt5%k45b%cx%%v)mng+"
